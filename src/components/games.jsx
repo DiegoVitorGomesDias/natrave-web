37720e78d591c88f3eb5797854184e7c181d051d
Diego V <diegovitorgomesdias@gmail.com>
2023-06-19 23:19:00 -0300

    auth

--- src/components/games.jsx
+++ src/components/games.jsx
@@ -1,51 +1,49 @@
 import "~/components/games.css";
 
 import axios from "axios";
-import { useFormik } from "formik";
+import { isObject, useFormik } from "formik";
 import * as yup from "yup";
 
-export const Games = ( {game} ) =>
-{
+export const Games = ({ game }) => {
     const auth = JSON.parse(localStorage.getItem("auth"));
     let { hour, teamOne, teamTwo, gameId } = game;
-    
+
     teamOne.slug = teamOne.slug ? teamOne.slug.toString().toLowerCase() : "-";
-    teamTwo.slug = teamTwo.slug ? teamTwo.slug.toString().toLowerCase () : "-";
+    teamTwo.slug = teamTwo.slug ? teamTwo.slug.toString().toLowerCase() : "-";
 
     const validationSchema = yup.object().shape
-    ({
-        homeTeamScore: yup.string(),
-        awayTeamScore: yup.string()
-    })
+        ({
+            homeTeamScore: yup.string(),
+            awayTeamScore: yup.string()
+        })
 
     const formik = useFormik
-    ({
-        onSubmit: async (values) => 
-        {
-            if (window.location.pathname !== ("/" + auth.user.username)) return
+        ({
+            onSubmit: async (values) => {
+                if (!isObject(auth)) return
 
-            values.homeTeamScore = values.homeTeamScore ||  0;
-            values.awayTeamScore = values.awayTeamScore || 0;
+                values.homeTeamScore = values.homeTeamScore || 0;
+                values.awayTeamScore = values.awayTeamScore || 0;
 
-            const res = await axios
-            ({
-                method: "post",
-                baseURL: import.meta.env.VITE_API_URL,
-                url: "/hunches",
-                headers:
-                {
-                    authorization: "Bearer " + auth.accessToken
-                },
-                data: { ...values, gameId }
-            })
-        },
-        initialValues:
-        {
-            "homeTeamScore": teamOne.score,
-            "awayTeamScore": teamTwo.score
-        },
-        validationSchema
-    })
+                const res = await axios
+                    ({
+                        method: "post",
+                        baseURL: import.meta.env.VITE_API_URL,
+                        url: "/hunches",
+                        headers:
+                        {
+                            authorization: "Bearer " + auth.accessToken
+                        },
+                        data: { ...values, gameId }
+                    })
+            },
+            initialValues:
+            {
+                "homeTeamScore": teamOne.score,
+                "awayTeamScore": teamTwo.score
+            },
+            validationSchema
+        })
 
     return (
         <div className="flex flex-col items-center justify-center border-2 border-gray-300 gap-2 rounded-2xl px-4 pt-3 pb-6 lg:px-12 lg:py-6">
@@ -55,26 +53,26 @@ export const Games = ( {game} ) =>
 
                 <span className="flex items-center justify-center gap-2">
                     <label htmlFor={teamOne.slug} className="text-gray-500 uppercase hidden sm:inline-block">{teamOne.slug}</label>
-                    <img src={`/assets/flags/${teamOne.slug}.png`} alt={teamOne.slug} className="inline-block w-12 md:w-max"/>
+                    <img src={`/assets/flags/${teamOne.slug}.png`} alt={teamOne.slug} className="inline-block w-12 md:w-max" />
 
-                    <input 
-                        type="number" name="homeTeamScore" 
-                        id={teamOne.slug} placeholder="-" 
+                    <input
+                        type="number" name="homeTeamScore"
+                        id={teamOne.slug} placeholder="-"
                         value={formik.values.homeTeamScore}
                         onChange={formik.handleChange}
                         onBlur={formik.handleSubmit}
                         className="placar inline-block w-12 h-12 md:w-14 md:h-14 rounded 
                         bg-red-300/30 outline-red-500/50 outline-1
-                        text-center placeholder-red-700 text-red-700" 
+                        text-center placeholder-red-700 text-red-700"
                     />
                 </span>
 
                 <span className="text-red-500 font-semibold">X</span>
 
                 <span className="flex items-center justify-center gap-2">
-                    <input 
-                        type="number" name="awayTeamScore" 
-                        id={teamTwo.slug} placeholder="-" 
+                    <input
+                        type="number" name="awayTeamScore"
+                        id={teamTwo.slug} placeholder="-"
                         value={formik.values.awayTeamScore}
                         onChange={formik.handleChange}
                         onBlur={formik.handleSubmit}
@@ -82,7 +80,7 @@ export const Games = ( {game} ) =>
                         bg-red-300/30 outline-red-500/50 outline-1
                         text-center placeholder-red-700 text-red-700"
                     />
-                    <img src={`/assets/flags/${teamTwo.slug}.png`} alt={teamTwo.slug} className="inline-block w-12 md:w-max"/>
+                    <img src={`/assets/flags/${teamTwo.slug}.png`} alt={teamTwo.slug} className="inline-block w-12 md:w-max" />
                     <label htmlFor={teamTwo.slug} className="text-gray-500 uppercase hidden sm:inline-block">{teamTwo?.slug}</label>
                 </span>
 
